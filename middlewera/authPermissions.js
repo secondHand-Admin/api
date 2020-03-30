a4d51b1c58245fff8389e679d093670b4b96feb4
Erro7 <tminly@qq.com>
2020-03-30 23:59:08 +0800

    文档

--- middlewera/authPermissions.js
+++ middlewera/authPermissions.js
@@ -1,14 +1,18 @@
 const permissionList = require('../config/permissions')
 //权限验证
 module.exports = (req, res, next) => {
-    let { host } = req.headers;
-    let { method, body, path } = req
-    let { userName } = body.userInfo
-    let dateLog = `Time:${Date()} User:${userName} Method:${method} url:${host}/${path}`
-    console.log('Log:', dateLog);
-    if (path.indexOf('/login') !== -1) return next()
-    let { leavel } = body.userInfo
-    let list = permissionList[leavel]
-    let boolean = false
+    try {
+        let { host } = req.headers;
+        let { method, body, path } = req
+        let { userName = '' } = body.userInfo
+        let dateLog = `Time:${Date()} User:${userName} Method:${method} url:${host + path}`
+        console.log('Log:', dateLog);
+        if (path.indexOf('/login') !== -1) return next()
+        let { leavel } = body.userInfo
+        let list = permissionList[leavel]
+        let boolean = false
+    } catch (error) {
+
+    }
     next()
 }
